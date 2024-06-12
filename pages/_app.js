8d8556ffb94aa037b317f18e9e9a08e2202c53ad
Glauber <glauber.bannwart@gmail.com>
2024-06-11 23:30:50 -0300

    Refactored _app.js to correctly set up WalletProvider.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import WalletProvider from '../components/WalletProvider';
-import '../styles/globals.css';
+import React from "react";
+import App from "next/app";
+import WalletProvider from "../components/WalletProvider";
+import "../styles/globals.css";
+import "../styles/App.css";
 
 function MyApp({ Component, pageProps }) {
   return (
